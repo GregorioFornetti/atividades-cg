182092c0282c0d37d423a0da3e721239995403e6
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-10-15 00:18:40 -0300

    tests vec3 vectorized

--- Atividade02/vectorized/Vec3.py
+++ Atividade02/vectorized/Vec3.py
@@ -471,9 +471,9 @@ class Vec3(Vec):
 
             - Vec3 - Resultado do produto vetorial.
         '''
-        return Vec3(self.vec[1] * other.vec[2] - self.vec[2] * other.vec[1],
+        return Vec3([self.vec[1] * other.vec[2] - self.vec[2] * other.vec[1],
                     self.vec[2] * other.vec[0] - self.vec[0] * other.vec[2],
-                    self.vec[0] * other.vec[1] - self.vec[1] * other.vec[0])
+                    self.vec[0] * other.vec[1] - self.vec[1] * other.vec[0]])
     
     def unit_vector(self) -> 'Vec3':
         '''
