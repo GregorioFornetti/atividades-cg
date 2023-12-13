5345df68c0a8628e9d95e6c534a0b488c8106bce
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-12-13 13:36:19 -0300

    Update utils.py

--- Atividade04/src/utils.py
+++ Atividade04/src/utils.py
@@ -1,33 +1,7 @@
-'''
-    Funções utilitárias que podem ser necessárias em qualquer parte do código.
-'''
-
-from Atividade04.src.constants import pi
-
-def degrees_to_radians(degrees: float):
-    '''
-    Converte um ângulo de graus para radianos.
-
-    ---
-
-    Parâmetros:
-
-        - degrees: float - Ângulo em graus.
-    
-    ---
-
-    Retorno:
-
-        - float - Ângulo em radianos.
-    '''
-    return degrees * pi / 180.0
-
-'''
-    Funções utilitárias que podem ser necessárias em qualquer parte do código.
-'''
 import random
 
 from Atividade04.src.constants import pi
+
 
 def degrees_to_radians(degrees: float):
     '''
